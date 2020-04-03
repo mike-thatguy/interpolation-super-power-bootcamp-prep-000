b0afa1981ef013826737d1ad6adabdaa4cb198d6
Mike Gretton <mikegretton@gmail.com>
2020-04-03 01:24:51 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,10 +1,10 @@
 # Write your #display_rainbow method here
 def display_rainbow(arr)
-  puts "#{arr[0][0,1].upcase}: #{arr[0]}, " + 
-    "#{arr[0][0,1].upcase}: #{arr[0]}, " + 
-    "#{arr[0][0,1].upcase}: #{arr[0]}, " + 
-    "#{arr[0][0,1].upcase}: #{arr[0]}, " + 
-    "#{arr[0][0,1].upcase}: #{arr[0]}, " + 
-    "#{arr[0][0,1].upcase}: #{arr[0]}, " + 
+  puts "#{arr[0][0,1].upcase}: #{arr[0]}, " +
+    "#{arr[0][0,1].upcase}: #{arr[0]}, " +
+    "#{arr[0][0,1].upcase}: #{arr[0]}, " +
+    "#{arr[0][0,1].upcase}: #{arr[0]}, " +
+    "#{arr[0][0,1].upcase}: #{arr[0]}, " +
+    "#{arr[0][0,1].upcase}: #{arr[0]}, " +
     "#{arr[0][0,1].upcase}: #{arr[0]}"
   end
